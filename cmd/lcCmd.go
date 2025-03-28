74b2f05a987175376ff84b7050d681fadba223e0
egouinguenet <elouangouinguenet@gmail.com>
2025-03-27 21:41:17 -0700

    bootstrap new command "line count"

---
+++ cmd/lcCmd.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"io/fs"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func NewLcCmd(fsys fs.FS) *cobra.Command {
+	lcCmd := &cobra.Command{
+		Use:        "lc",
+		Aliases:    []string{"line_count", "lineCount", "LineCount", "Linecount", "linecount"},
+		Example:    "dinf lc",
+		SuggestFor: []string{"cl"},
+		Short:      "Count lines in files of the current directory.",
+		Args:       cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return nil
+		},
+	}
+
+	return lcCmd
+}
+
+func init() {
+	wd, _ := os.Getwd()
+	fsys := os.DirFS(wd)
+	lcCmd := NewLcCmd(fsys)
+	rootCmd.AddCommand(lcCmd)
+}
