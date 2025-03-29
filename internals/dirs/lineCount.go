21a6673403922bb487f442ad40720fe0bacd7b11
egouinguenet <elouangouinguenet@gmail.com>
2025-03-29 14:03:49 -0700

    green empty file system

--- internals/dirs/lineCount.go
+++ internals/dirs/lineCount.go
@@ -1,7 +1,39 @@
 package dirs
 
-import "io/fs"
+import (
+	"bufio"
+	"io/fs"
+	"os"
+)
 
 func LineCount(fsys fs.FS) (int, error) {
-	return 0, nil
+	dirents, err := fs.ReadDir(fsys, ".")
+	if err != nil {
+		return 0, err
+	}
+
+	lc := 0
+	for _, d := range dirents {
+		// Ignore directories, they don't have line per say.
+		if d.IsDir() {
+			continue
+		}
+
+		f, err := os.Open(d.Name())
+		// FIX: Implement test case for this error
+		if err != nil {
+			return 0, err
+		}
+		defer f.Close()
+
+		s := bufio.NewScanner(f)
+		s.Split(bufio.ScanLines)
+		for s.Scan() {
+			// From bufio.ScanLines docs: The returned line may be empty.
+			// Might be worth to ignore empty line... Maybe create an option.
+			lc += 1
+		}
+	}
+
+	return lc, nil
 }
--- internals/dirs/lineCount_test.go
+++ internals/dirs/lineCount_test.go
@@ -2,35 +2,67 @@ package dirs_test
 
 import (
 	"fmt"
+	"os"
 	"testing"
-	"testing/fstest"
 
 	"github.com/Skylli202/dinf/internals/dirs"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestLineCount(t *testing.T) {
-	cases := []struct {
-		fs                 fstest.MapFS
-		msg                string
-		expectedLineCount  int
-		expectedLineCountR int
-	}{
-		{
-			fs:                 fstest.MapFS{},
-			msg:                "Empty file system.",
-			expectedLineCount:  0,
-			expectedLineCountR: 0,
-		},
-	}
+	t.Run("Empty dir", func(t *testing.T) {
+		emptyDirName, err := os.MkdirTemp("", "")
+		if err != nil {
+			t.Fatal("Unable to `os.MkdirTemp(...)`. Unable to execute tests.")
+		}
+		defer os.RemoveAll(emptyDirName)
 
-	for i, tc := range cases {
-		lc, err := dirs.LineCount(tc.fs)
-		// lcR, errR := dirs.LineCountR(fsys fs.FS)
+		msg := "Empty file system."
+		expectedLineCount := 0
 
-		assert.Nil(t, err, fmt.Sprintf("LineCount should return err equals to Nil, but it is not for test case: [%d] %s", i, tc.msg))
+		fs := os.DirFS(emptyDirName)
+		lc, err := dirs.LineCount(fs)
+
+		assert.Nil(t, err, fmt.Sprintf("LineCount should return err equals to Nil, but it is not for test case: [%d] %s", 999, msg))
 		// assert.Nil(t, errR, fmt.Sprintf("LineCountR should return err equals to Nil, but it is not for test case: [%d] %s", i, tc.msg))
-		assert.Equal(t, tc.expectedLineCount, lc, fmt.Sprintf("Line count does not match.\nTest case: %d - %q.\nTest case's file system:\n%+v\n", i, tc.msg, tc.fs))
+		assert.Equal(t, expectedLineCount, lc, fmt.Sprintf("Line count does not match.\nTest case: %d - %q.\nTest case's file system:\n%+v\n", 999, msg, emptyDirName))
 		// assert.Equal(t, tc.expectedSizeR, sizeR, fmt.Sprintf("Line count R  does not match.\nTest case: %d - %q.\nTest case's file system:\n%+v\n", i, tc.msg, tc.fs))
-	}
+	})
+	// dname, err := os.MkdirTemp("", "testlinecount")
+	// if err != nil {
+	// 	t.Fatal("Unable to `os.MkdirTemp(...)`. Unable to execute tests.")
+	// }
+	// defer os.RemoveAll(dname)
+
+	// file1, err := os.CreateTemp("", "")
+	// cases := []struct {
+	// 	fs                 fstest.MapFS
+	// 	msg                string
+	// 	expectedLineCount  int
+	// 	expectedLineCountR int
+	// }{
+	// 	{
+	// 		fs:                 fstest.MapFS{},
+	// 		msg:                "Empty file system.",
+	// 		expectedLineCount:  0,
+	// 		expectedLineCountR: 0,
+	// 	},
+	// 	{
+	// 		fs: fstest.MapFS{
+	// 			"foo": &fstest.MapFile{Data: []byte("123")},
+	// 		},
+	// 		msg:                "A single line in a single file (no subdirectories).",
+	// 		expectedLineCount:  1,
+	// 		expectedLineCountR: 1,
+	// 	},
+	// }
+	// for i, tc := range cases {
+	// 	lc, err := dirs.LineCount(tc.fs)
+	// 	// lcR, errR := dirs.LineCountR(fsys fs.FS)
+	//
+	// 	assert.Nil(t, err, fmt.Sprintf("LineCount should return err equals to Nil, but it is not for test case: [%d] %s", i, tc.msg))
+	// 	// assert.Nil(t, errR, fmt.Sprintf("LineCountR should return err equals to Nil, but it is not for test case: [%d] %s", i, tc.msg))
+	// 	assert.Equal(t, tc.expectedLineCount, lc, fmt.Sprintf("Line count does not match.\nTest case: %d - %q.\nTest case's file system:\n%+v\n", i, tc.msg, tc.fs))
+	// 	// assert.Equal(t, tc.expectedSizeR, sizeR, fmt.Sprintf("Line count R  does not match.\nTest case: %d - %q.\nTest case's file system:\n%+v\n", i, tc.msg, tc.fs))
+	// }
 }
