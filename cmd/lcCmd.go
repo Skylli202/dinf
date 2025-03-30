201bb8dccb0390b15ec972ce1d69f0f1174ef079
egouinguenet <elouangouinguenet@gmail.com>
2025-03-29 17:44:01 -0700

    wip path as first args

--- cmd/lcCmd.go
+++ cmd/lcCmd.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"errors"
+	"fmt"
+	"io/fs"
 	"os"
 
 	"github.com/Skylli202/dinf/internals"
@@ -14,8 +17,36 @@ func NewLcCmd(dirname string) *cobra.Command {
 		Example:    "dinf lc",
 		SuggestFor: []string{"cl"},
 		Short:      "Count lines in files of the current directory.",
-		Args:       cobra.NoArgs,
+		Args:       cobra.MinimumNArgs(1),
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			p := args[0]
+			_, err := os.Stat(p)
+			if err != nil {
+				if errors.Is(err, fs.ErrNotExist) {
+					return fmt.Errorf("file, or directory, \"%s\" does not exist", p)
+				}
+				return err
+			}
+			args[0] = "/home/egouinguenet/"
+			// if path.IsAbs(p) {
+			// 	if fs.ValidPath(p) {
+			// 		fmt.Println(p, "is a valid and absolute path.")
+			// 	} else {
+			// 		fmt.Println(p, "is invalid and absolute path.")
+			// 	}
+			// } else {
+			// 	if fs.ValidPath(p) {
+			// 		fmt.Println(p, "is a valid and relative path.")
+			// 	} else {
+			// 		fmt.Println(p, "is invalid and relative path.")
+			// 	}
+			// }
+			// fmt.Println("Clean:", path.Clean(p))
+			//
+			return nil
+		},
 		RunE: func(cmd *cobra.Command, args []string) error {
+			fmt.Println("args[0]:", args[0])
 			w := cmd.OutOrStdout()
 
 			raw, err := cmd.Flags().GetBool("raw")
